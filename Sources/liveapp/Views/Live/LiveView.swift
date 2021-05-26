ae73c691c673d8441c629289710a9b279df66654
Joe Hinkle <joehinkle11@gmail.com>
2021-05-26 12:39:21 -0600

    custom dependencies

---
+++ Sources/liveapp/Views/Live/LiveView.swift
@@ -0,0 +1,126 @@
+//
+//  LiveView.swift
+//  LiveApp
+//
+//  Created by Joseph Hinkle on 4/6/21.
+//
+
+import SwiftUI
+#if _BUILD_FROM_SOURCE
+import SwiftUIInterpreter
+#else
+import FlattenedSwiftUIInterpreter
+#endif
+import ExceptionCatcher
+
+#if PRODUCTION
+public protocol LiveView: LiveUI {
+    associatedtype LiveBody = View
+//    var source: LiveSource { get } // todo
+    @ViewBuilder var liveBody: LiveBody { get }
+}
+#else
+public protocol LiveView: LiveUI, _IsLiveView {
+    associatedtype LiveBody = View
+//    var source: LiveSource { get } // todo
+    @ViewBuilder var liveBody: LiveBody { get }
+}
+public protocol _IsLiveView {}
+#endif
+public protocol LiveUI: View {
+    var _internal: _InternalLiveUIData { get }
+}
+
+public struct _InternalLiveUIData {
+    let compiledViewGetter: () -> AnyView
+    let protocolName: String
+}
+
+extension LiveView where LiveBody: View {
+    /// Force refreshes this live view to download the latest data from its remote repository.
+    /// - Parameter upgrade: Dictates how the live view should upgrade to the latest version if a newer version is downloaded. Passing nil defaults to value in `LiveApp.Configuration.defaultUpgradeLogic`.
+    static func refresh(upgrade: UpgradeLogic? = nil) {
+        fatalError("todo")
+    }
+//    public var source: LiveSource {
+//        fatalError()
+////        switch LiveApp.Configuration.projectDefaultSource {
+////        case .none:
+////            return .none
+////        case .liveAppDataServer, .selfHost:
+////            let structName = String(describing: Self.self)
+////            return .remoteRepository(liveViewName: structName)
+////        }
+//    }
+    public var _internal: _InternalLiveUIData {
+        .init(compiledViewGetter: {.init(liveBody)}, protocolName: "LiveView")
+    }
+}
+extension LiveUI {
+    #if INCLUDE_DEVELOPER_TOOLS
+    func buildBody() throws -> AnyView {
+        if _internal.protocolName == "UILiveViewRepresentablex" {
+            return AnyView(_internal.compiledViewGetter())
+        }
+        return try ExceptionCatcher.catch {
+            try buildStruct(
+                for: self,
+                allowDebugMessages: LiveApp.Configuration.showDeveloperMessages,
+                getBackupView: {
+                    var backupView = _internal.compiledViewGetter()
+                    if let outlineColor =
+                        LiveApp.outlineCompiledViewsColor {
+                        backupView = AnyView(backupView.border(outlineColor, width: 1))
+                    }
+                    if LiveApp.Configuration.showDeveloperMessages {
+                        return AnyView(backupView.overlay(NotSetupView()))
+                    } else {
+                        return AnyView(backupView)
+                    }
+                },
+                applyModifier: { interpretedView in
+                    if let outlineColor =
+                        LiveApp.outlineInterpretedViewsColor {
+                        return AnyView(
+                            interpretedView.border(outlineColor, width: 1)
+                        )
+                    } else {
+                        return interpretedView
+                    }
+                }
+            )
+        }
+    }
+    #else
+    func buildBody() throws -> AnyView {
+        try ExceptionCatcher.catch {
+            try buildStruct(
+                for: self,
+                allowDebugMessages: false,
+                getBackupView: {
+                    compiledView
+                }
+            )
+        }
+    }
+    #endif
+    #if INCLUDE_DEVELOPER_TOOLS
+    /// Recommended for development environments. When built with developer tools, any failure to interpreter the live data will notify the developer with a UI pop up. This helps developers catch configuration issues with Live App quickly.
+    public var body: some View {
+        do {
+            return try buildBody()
+        } catch {
+            if LiveApp.Configuration.showDeveloperMessages {
+                return AnyView(_internal.compiledViewGetter().overlay(NotSetupView()))
+            } else {
+                return AnyView(_internal.compiledViewGetter())
+            }
+        }
+    }
+    #else
+    /// Recommended for production environments. When built without developer tools, any failure to interpreter the live data will default to rendering the normal compiled SwiftUI view. This ensures that the end user never sees an error message regarding any Live App errors.
+    public var body: some View {
+        (try? buildBody()) ?? AnyView(liveBody)
+    }
+    #endif
+}
---
+++ Sources/liveapp/Views/Live/UILiveViewRepresentable.swift
@@ -0,0 +1,57 @@
+//
+//  UILiveViewRepresentable.swift
+//  
+//
+//  Created by Joseph Hinkle on 4/10/21.
+//
+
+import SwiftUI
+
+@available(iOS 13.0, tvOS 13.0, *)
+@available(macOS, unavailable)
+@available(watchOS, unavailable)
+public protocol UILiveViewRepresentable : LiveUI {
+    associatedtype UIViewType : UIView
+    func makeUIView(context: Self.Context) -> Self.UIViewType
+    func updateUIView(_ uiView: Self.UIViewType, context: Self.Context)
+    static func dismantleUIView(_ uiView: Self.UIViewType, coordinator: Self.Coordinator)
+    associatedtype Coordinator = Void
+    func makeCoordinator() -> Self.Coordinator
+    typealias Context = UIViewRepresentableContext<_UIWildcardViewRepresentable<Self>>
+    
+    // Live App inventions
+    associatedtype RegistrationStatus : RegisterWithInterpreter
+    static var registerWithInterpreter: Self.RegistrationStatus { get }
+}
+
+public protocol RegisterWithInterpreter {}
+public struct DoRegisterWithInterpreter: RegisterWithInterpreter {}
+public struct DontRegisterWithInterpreter: RegisterWithInterpreter {}
+
+extension UILiveViewRepresentable {
+    public var _internal: _InternalLiveUIData {
+        .init(compiledViewGetter: {.init(_UIWildcardViewRepresentable(self))}, protocolName: "UILiveViewRepresentable")
+    }
+    
+    public static var Yes: RegisterWithInterpreter {
+//        RegisterWithInterpreter(value: true)
+        fatalError()
+    }
+    public static var No: RegisterWithInterpreter {
+//        RegisterWithInterpreter(value: false)
+        fatalError()
+    }
+}
+@available(iOS 13.0, tvOS 13.0, watchOS 6.0, *)
+@available(macOS, unavailable)
+public extension UILiveViewRepresentable {
+    static func dismantleUIView(_ uiView: Self.UIViewType, coordinator: Self.Coordinator) {
+        fatalError()
+    }
+    var liveBody: Never {
+        fatalError()
+    }
+    func makeCoordinator() -> Self.Coordinator {
+        fatalError()
+    }
+}
