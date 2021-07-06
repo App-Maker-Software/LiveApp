d2680037e8a0c8a0fad2559d5c36be682f45d05c
Joe Hinkle <joehinkle11@gmail.com>
2021-07-05 20:25:10 -0600

    interpreter 0.4.2

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         // todo: move over to the public binary distribution
-        .package(name: "SwiftInterpreter", url: "https://github.com/App-Maker-Software/SwiftInterpreter", from: "0.4.1"),
+        .package(name: "SwiftInterpreter", url: "https://github.com/App-Maker-Software/SwiftInterpreter", from: "0.4.2"),
 //        .package(name: "SwiftInterpreterSource", url: "https://github.com/App-Maker-Software/SwiftInterpreterSource", .branch("main")), // for private development work
         .package(name: "ExceptionCatcher", url: "https://github.com/sindresorhus/ExceptionCatcher", from: "2.0.0"),
     ],
