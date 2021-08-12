411d49091e98da6b6d6691666f5d0868bd79d3b4
Joe Hinkle <joehinkle11@gmail.com>
2021-08-12 17:02:03 -0400

    new interpreter, 0.4.0 attempt

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         // todo: move over to the public binary distribution
-        .package(name: "SwiftInterpreter", url: "https://github.com/App-Maker-Software/SwiftInterpreter", from: "0.4.5"),
+        .package(name: "SwiftInterpreter", url: "https://github.com/App-Maker-Software/SwiftInterpreter", from: "0.4.6"),
 //        .package(name: "SwiftInterpreterSource", url: "https://github.com/App-Maker-Software/SwiftInterpreterSource", .branch("main")), // for private development work
         .package(name: "ExceptionCatcher", url: "https://github.com/sindresorhus/ExceptionCatcher", from: "2.0.0"),
         .package(url: "https://github.com/wickwirew/Runtime.git", from: "2.2.2"),
@@ -27,7 +27,7 @@ let package = Package(
             dependencies: [
                 "ExceptionCatcher",
                 "Runtime",
-                .product(name: "SwiftInterpreter", package: "SwiftInterpreter")
+                .product(name: "SwiftInterpreter", package: "SwiftInterpreter"),
 //                .product(name: "SwiftInterpreterDebugOnly", package: "SwiftInterpreter") // unfortunately, this still isn't working. we must bring the xcframework into production builds even if we aren't using it
             ],
             swiftSettings: [
